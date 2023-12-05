b4cd2378f96066845786818b6360e81eedb7531e
zaynabkrm <zaynabelkaroumi@gmail.com>
2023-12-05 07:04:17 -0800

    file

--- main.h
+++ main.h
@@ -1,11 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
 int printf_char(va_list val);
-int _putchar(char c);
 int _printf(const char *format, ...);
 int printf_string(va_list val);
 int _strlen(char *str);
 int _strlen(const char *str);
+void handle_char(int c, int *count);
 
+int _putchar(char c);
 #endif
